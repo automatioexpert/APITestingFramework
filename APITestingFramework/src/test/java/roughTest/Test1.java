a7095830c3cdf09f3015a45b0ea76155ee396787
Valmiki Vishwakarma <you@example.com>
2022-12-08 22:28:12 +0530

    Add.

---
+++ APITestingFramework/src/test/java/roughTest/Test1.java
@@ -0,0 +1,15 @@
+package roughTest;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		String str = "Thank You God!!";
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(str + " : " + i);
+		}
+
+	}
+
+}
